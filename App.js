5dc7677e05acc47318ff9a7685b8bbb575338fb8
tinhlv44 <98213200+tinhlv44@users.noreply.github.com>
2024-09-09 15:16:24 +0700

    push finish

--- App.js
+++ App.js
@@ -15,39 +15,46 @@ const App = () => {
         backgroundColor={isPortrait ? '#fff' : '#000'}
       />
       <View style={{flexDirection: !isPortrait ? 'row' : 'column'}}>
-        <TouchableOpacity style={[styles.button, { width: isPortrait ? width * 0.5 : width * 0.15 }]}>
+        <TouchableOpacity 
+          style={[styles.button, { width: isPortrait ? width * 0.5 : width * 0.15 }]}
+          onPress={() => alert('Đã nhấn vào nút 1')}
+        >
           <Text style={styles.text}>
             Nút 1
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, { width: isPortrait ? width * 0.5 : width * 0.15 }]}>
+        <TouchableOpacity 
+          style={[styles.button, { width: isPortrait ? width * 0.5 : width * 0.15 }]}
+          onPress={() => alert('Đã nhấn vào nút 2')}
+        >
           <Text style={styles.text}>
             Nút 2
           </Text>
         </TouchableOpacity>
       </View>
-      
-      <Image
-        source={{ uri: 'https://cdn.pixabay.com/photo/2024/01/31/19/25/sunset-8544672_640.jpg' }}
-        style={{ width: imageWidth, height: imageHeight}}
-      />
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={width/2}
-      >
-        <TextInput 
-          placeholder="Nhập văn bản" 
-          style={[styles.textInput, { width: isPortrait ? width * 0.8 : width * 0.2 }]}  
+      <View>
+        <Image
+          source={{ uri: 'https://cdn.pixabay.com/photo/2024/01/31/19/25/sunset-8544672_640.jpg' }}
+          style={{ width: imageWidth, height: imageHeight}}
         />
-      </KeyboardAvoidingView>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          keyboardVerticalOffset={width/2}
+        >
+          <TextInput 
+            placeholder="Lê Văn Tính - 2124801030225" 
+            style={[styles.textInput, { width: imageWidth }]}  
+          />
+        </KeyboardAvoidingView>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container:{
-    //flex: 1
+    marginTop:10
   },
   portraitContainer: {
     flexDirection: 'column',
